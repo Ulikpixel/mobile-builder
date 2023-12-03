0616a3c54dcc6125d4c3c5f483ad341e75d36dcd
Ivan <frontend@imin.pro>
2023-12-03 22:55:58 +0600

    feat: update

--- app/[...missing].tsx
+++ app/[...missing].tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react'
-import { Link, Stack } from 'expo-router'
+import { Stack } from 'expo-router'
 import { Text, View } from 'react-native'
 
 const NotFoundScreen: FC = () => (
@@ -7,9 +7,6 @@ const NotFoundScreen: FC = () => (
     <Stack.Screen options={{ title: 'Oops!' }} />
     <View>
       <Text>This screen doesnt exist.</Text>
-      <Link href="/">
-        <Text>Go to home screen!</Text>
-      </Link>
     </View>
   </>
 )
